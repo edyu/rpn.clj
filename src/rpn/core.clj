ffe11a0be0d92431cdc194437c416eb987efef25
Ed Yu <ed.yu@yahoo.com>
2013-03-28 16:08:56 -0700

    Basic command prompt

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -1,6 +1,11 @@
 (ns rpn.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn runloop []
+  (loop [line (read-line)]
+    (if (or (nil? line) (= line "q"))
+      (println "goodbye")
+      (do
+        (println line)
+        (recur (read-line))))))
+
+(defn -main [] (runloop))
