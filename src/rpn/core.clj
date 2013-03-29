213272885583d7697dd0e110cd695ee3626c7ed9
Ed Yu <ed.yu@yahoo.com>
2013-03-28 21:36:17 -0700

    Add simple math

--- src/rpn/core.clj
+++ src/rpn/core.clj
@@ -1,11 +1,35 @@
 (ns rpn.core)
 
+(defn str->num [str]
+  (if (number? str)
+    str
+    (let [n (read-string str)]
+      (if (number? n) n nil))))
+
+(defn do-simple-math [stack]
+  (try
+    (apply (eval (symbol (first stack))) (map #(str->num %) (take 2 (rest stack))))
+    (catch Exception e (println e))))
+
+(defn calculate [stack]
+  (let [op (first stack)]
+    (cond
+      (re-find #"\+|-|\*|/" op)
+        (conj (drop 3 stack)
+          (do-simple-math stack))
+      :else
+        stack)))
+
 (defn runloop []
-  (loop [line (read-line)]
+  (loop [line (read-line)
+         stack ()]
     (if (or (nil? line) (= line "q"))
       (println "goodbye")
-      (do
-        (println line)
-        (recur (read-line))))))
+      (let [op line
+            stack (calculate (conj stack op))]
+        (do
+          (println (first stack))
+          (println (str "stack: " stack))
+          (recur (read-line) stack))))))
 
 (defn -main [] (runloop))
